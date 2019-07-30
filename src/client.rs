2c118bb438c6c5040640129496088ac9eaaf2ef3
CuriouslyCurious <thecuriouslycurious@protonmail.com>
2019-07-30 13:36:42 +0200

    Add authenticated_request to client.rs

--- src/client.rs
+++ src/client.rs
@@ -53,13 +53,38 @@ impl Client {
     /// as endpoint.
     pub fn request(&self, url: &str) -> reqwest::Result<reqwest::Response> {
         let full_url = format!("{base_url}/{url}", base_url=BASE_URL, url=url);
+        let headers = self.create_lang_header();
 
+        self.client.get(&full_url).headers(headers).send()
+    }
+
+    /// Make an authenticated request to the Guild Wars 2 API with the given url (which has to
+    /// include version) as endpoint. This requires that the `api_key` field of the client is set,
+    /// otherwise it panics.
+    ///
+    /// This function may fail depending on what the settings of the API key itself are, since you
+    /// can limit what resources a certain key may access. In that case the function will return
+    /// an error.
+    pub fn authenticated_request(&self, url: &str) -> reqwest::Result<reqwest::Response> {
+        let full_url = format!("{base_url}/{url}", base_url=BASE_URL, url=url);
+        let mut headers = self.create_lang_header();
+
+        // Create authorization header
+        let api_key = self.api_key().expect("Guild Wars 2 API key is not set").to_owned();
+        headers.insert(reqwest::header::AUTHORIZATION, format!("Bearer {}", api_key).parse().unwrap());
+
+        self.client.get(&full_url).headers(headers).send()
+    }
+
+    /// Creates a language HTTP header from the client's given language, if no language is given it
+    /// will default to English.
+    fn create_lang_header(&self) -> HeaderMap {
         // Defaults to English if no language is specified
         let lang = self.lang().unwrap_or(&Localisation::English).to_string();
 
         let mut headers = HeaderMap::new();
         headers.insert(reqwest::header::ACCEPT_LANGUAGE, lang.parse().unwrap());
-        self.client.get(&full_url).headers(headers).send()
+        headers
     }
 
     /// Returns an `Option` containing a string slice of the Guild Wars 2 API key for the
