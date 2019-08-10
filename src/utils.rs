ae3e081975d382650310d131d93e550a4934d9df
CuriouslyCurious <thecuriouslycurious@protonmail.com>
2019-08-10 11:31:26 +0200

    Made ids_to_string generic

--- src/utils.rs
+++ src/utils.rs
@@ -2,8 +2,8 @@ use reqwest::{StatusCode, Response};
 use serde::de::DeserializeOwned;
 use crate::error::ApiError;
 
-/// Convert a `Vec<u32>` to a comma-separated `String`
-pub fn ids_to_string(ids: Vec<u32>) -> String {
+/// Convert a `Vec<T>` to a comma-separated `String`
+pub fn ids_to_string(ids: Vec<impl ToString>) -> String {
     let mut ids: String = ids.iter().map(|id| format!("{},", id.to_string())).collect();
     ids.pop(); // Remove the last comma
     ids
