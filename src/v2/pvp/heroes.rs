8c0b13f35178ee67e062c50406304d54c3266f2d
CuriouslyCurious <thecuriouslycurious@protonmail.com>
2019-08-17 23:04:15 +0200

    Add further tests to pvp/heroes

--- src/v2/pvp/heroes.rs
+++ src/v2/pvp/heroes.rs
@@ -168,39 +168,98 @@ mod tests {
     use crate::client::Client;
     use crate::error::ApiError;
 
-    const JSON_HERO: &str = r#""
-        {
-            "id": "115C140F-C2F5-40EB-8EA2-C3773F2AE468",
-            "name": "Nika",
-            "description": "Nika was a proficient assassin schooled in her youth at Shing Jea Monastery. She served Cantha as a member of the Obsidian Flame.",
-            "type": "Specialist Hero",
-            "stats": {
-                "offense": 3,
-                "defense": 2,
-                "speed": 4
+    const JSON_HERO: &str = r#"
+    {
+        "id": "115C140F-C2F5-40EB-8EA2-C3773F2AE468",
+        "name": "Nika",
+        "description": "Nika was a proficient assassin schooled in her youth at Shing Jea Monastery. She served Cantha as a member of the Obsidian Flame.",
+        "type": "Specialist Hero",
+        "stats": {
+            "offense": 3,
+            "defense": 2,
+            "speed": 4
+        },
+        "overlay": "https://render.guildwars2.com/file/2CACF4120E370D1997A4C3D69BF592D7CC1870C8/993693.png",
+        "underlay": "https://render.guildwars2.com/file/103108E0D8EDD22C577FA4171618D004A82AD955/993694.png",
+        "skins": [
+            {
+                "id": 1,
+                "name": "Nika",
+                "icon": "https://render.guildwars2.com/file/4602BDC15B73422011AC664425D93750707F04F3/1058576.png",
+                "default": true
             },
-            "overlay": "https://render.guildwars2.com/file/2CACF4120E370D1997A4C3D69BF592D7CC1870C8/993693.png",
-            "underlay": "https://render.guildwars2.com/file/103108E0D8EDD22C577FA4171618D004A82AD955/993694.png",
-            "skins": [
-                {
-                    "id": 1,
-                    "name": "Nika",
-                    "icon": "https://render.guildwars2.com/file/4602BDC15B73422011AC664425D93750707F04F3/1058576.png",
-                    "default": true
-                },
-                {
-                    "id": 7,
-                    "name": "Shadow Assassin Nika",
-                    "icon": "https://render.guildwars2.com/file/01643F1BD1202007BEE8E37F7DA3EA31AEE9536C/1322841.png",
-                    "default": false
-                }
-            ]
-        }"#;
+            {
+                "id": 7,
+                "name": "Shadow Assassin Nika",
+                "icon": "https://render.guildwars2.com/file/01643F1BD1202007BEE8E37F7DA3EA31AEE9536C/1322841.png",
+                "default": false
+            }
+        ]
+    }"#;
 
-        match serde_json::from_str::<Hero>(json_hero) {
+    const JSON_STATS: &str = r#"
+    {
+        "offense": 3,
+        "defense": 2,
+        "speed": 4
+    }"#;
+
+    const JSON_SKIN: &str = r#"
+    {
+        "id": 1,
+        "name": "Nika",
+        "icon": "https://render.guildwars2.com/file/4602BDC15B73422011AC664425D93750707F04F3/1058576.png",
+        "default": true
+    }"#;
+
+    #[test]
+    fn create_hero() {
+        match serde_json::from_str::<Hero>(JSON_HERO) {
             Ok(_) => assert!(true),
             Err(e) => panic!(e.to_string()),
         }
+    }
+
+    #[test]
+    fn create_stats() {
+        match serde_json::from_str::<Stats>(JSON_STATS) {
+            Ok(_) => assert!(true),
+            Err(e) => panic!(e.to_string()),
+        }
+    }
+
+    #[test]
+    fn create_skin() {
+        match serde_json::from_str::<Skin>(JSON_SKIN) {
+            Ok(_) => assert!(true),
+            Err(e) => panic!(e.to_string()),
+        }
+    }
+
+    #[test]
+    fn accessors() {
+        let hero = serde_json::from_str::<Hero>(JSON_HERO).unwrap();
+        assert_eq!("115C140F-C2F5-40EB-8EA2-C3773F2AE468", hero.id());
+        assert_eq!("Nika", hero.name());
+        assert_eq!("Nika was a proficient assassin schooled in her youth at Shing Jea Monastery. She served Cantha as a member of the Obsidian Flame.", hero.description());
+        assert_eq!("Specialist Hero", hero.flavor_type());
+        // Stats
+        let stats = serde_json::from_str::<Stats>(JSON_STATS).unwrap();
+        assert_eq!(&stats, hero.stats());
+        println!("{:#?}", hero.stats());
+        assert_eq!(3, hero.stats().offense());
+        assert_eq!(2, hero.stats().defense());
+        assert_eq!(4, hero.stats().speed());
+        assert_eq!("https://render.guildwars2.com/file/2CACF4120E370D1997A4C3D69BF592D7CC1870C8/993693.png", hero.overlay_url());
+        assert_eq!("https://render.guildwars2.com/file/103108E0D8EDD22C577FA4171618D004A82AD955/993694.png", hero.underlay_url());
+        // Skins
+        let skin = serde_json::from_str::<Skin>(JSON_SKIN).unwrap();
+        let hero_skin = hero.skins().first().unwrap();
+        assert_eq!(&skin, hero_skin);
+        assert_eq!(1, hero_skin.id());
+        assert_eq!("Nika", hero_skin.name());
+        assert_eq!("https://render.guildwars2.com/file/4602BDC15B73422011AC664425D93750707F04F3/1058576.png", hero_skin.icon_url());
+        assert_eq!(true, hero_skin.default());
     }
 }
 
