310118e1089436d1bedcca977f86fab344327182
CuriouslyCurious <thecuriouslycurious@protonmail.com>
2022-03-27 13:12:51 +0200

    Replace ureq with minreq

--- src/client.rs
+++ src/client.rs
@@ -1,11 +1,14 @@
-use ureq::{Header, Response};
-use crate::error::ApiError;
+use minreq::Response;
 use serde::de::DeserializeOwned;
+
+use std::fmt::{self, Display};
+
+use crate::error::{ApiError, ApiErrorKind};
 
 // Base url to the GW2 API.
 pub const BASE_URL: &str = "https://api.guildwars2.com";
 // Wait max 10 seconds for a response from the server.
-pub const TIMEOUT: u64 = 10_000;
+pub const TIMEOUT: u64 = 10;
 
 /// All available localisations that are supported by the official Guild Wars 2 API.
 #[derive(Debug, PartialEq)]
@@ -17,16 +20,22 @@ pub enum Localisation {
     Chinese,
 }
 
-impl ToString for Localisation {
-    /// Converts the `Localisation` to a valid localisation suffix `String` for the Guild Wars 2 API.
-    fn to_string(&self) -> String {
+/// Converts the `Localisation` to a valid localisation suffix `String` for the Guild Wars 2 API.
+impl Display for Localisation {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            Localisation::English => "en".to_string(),
-            Localisation::Spanish => "es".to_string(),
-            Localisation::German => "de".to_string(),
-            Localisation::French => "fr".to_string(),
-            Localisation::Chinese => "zh".to_string(),
+            Localisation::English => f.write_str("en"),
+            Localisation::Spanish => f.write_str("es"),
+            Localisation::German => f.write_str("de"),
+            Localisation::French => f.write_str("fr"),
+            Localisation::Chinese => f.write_str("zh"),
         }
+    }
+}
+
+impl Default for Localisation {
+    fn default() -> Self {
+        Localisation::English
     }
 }
 
@@ -37,7 +46,7 @@ pub struct Client {
     api_key: Option<String>,
     /// The language that the response will be in. Defaults to English if left empty as per the
     /// official Guild Wars 2 API behvaiour.
-    lang: Option<Localisation>,
+    lang: Localisation,
 }
 
 impl Client {
@@ -45,7 +54,7 @@ impl Client {
     pub fn new() -> Client {
         Client {
             api_key: None,
-            lang: None,
+            lang: Localisation::default(),
         }
     }
 
@@ -57,35 +66,35 @@ impl Client {
 
     /// Sets the language to be used in responses, applies to item names and what not.
         pub fn set_lang(mut self, lang: Localisation) -> Client {
-        self.lang = Some(lang);
+        self.lang = lang;
         self
     }
 
-    /// Creates a language HTTP header from the client's given language, if no language is given it
-    /// will default to English.
-    fn create_lang_header(&self) -> Header {
-        let lang = self.lang().unwrap_or(&Localisation::English).to_string();
-        Header::new("Accept-Language", &lang)
-    }
-
-    /// Creates a HTTP authorization header from the client's given API key, if no key is set it
-    /// will panic.
-    fn create_auth_header(&self) -> Header {
-        let api_key = self.api_key().expect("Guild Wars 2 API key is not set").to_owned();
-        Header::new("Authorization", &format!("Bearer {}", api_key))
-    }
+    // /// Creates a language HTTP header from the client's given language, if no language is given it
+    // /// will default to English.
+    // fn create_lang_header(&self) -> Header {
+    //     let lang = self.lang().unwrap_or(&Localisation::English).to_string();
+    //     Header::new("Accept-Language", &lang)
+    // }
+    //
+    // /// Creates a HTTP authorization header from the client's given API key, if no key is set it
+    // /// will panic.
+    // fn create_auth_header(&self) -> Header {
+    //     let api_key = self.api_key().expect("Guild Wars 2 API key is not set").to_owned();
+    //     Header::new("Authorization", &format!("Bearer {}", api_key))
+    // }
 
     /// Make a request to the Guild Wars 2 API with the given url (which has to include version)
     /// as endpoint.
     pub fn request<T>(&self, url: &str) -> Result<T, ApiError>
     where T: DeserializeOwned {
         let full_url = format!("{base_url}/{url}", base_url=BASE_URL, url=url);
-        let lang_header = self.create_lang_header();
-        let response = ureq::get(&full_url)
-            .set(lang_header.name(), lang_header.value())
-            .timeout_connect(TIMEOUT)
-            .timeout_read(TIMEOUT)
-            .call();
+        //let lang_header = self.create_lang_header();
+        let response = minreq::get(&full_url)
+            .with_header("Accept-Language", self.lang.to_string())
+            //.set(lang_header.name(), lang_header.value())
+            .with_timeout(TIMEOUT)
+            .send()?;
         Client::handle_response(response)
     }
 
@@ -99,15 +108,13 @@ impl Client {
     pub fn authenticated_request<T>(&self, url: &str) -> Result<T, ApiError>
     where T: DeserializeOwned {
         let full_url = format!("{base_url}/{url}", base_url=BASE_URL, url=url);
-        let lang_header = self.create_lang_header();
-        let auth_header = self.create_auth_header();
 
-        let response = ureq::get(&full_url)
-            .set(lang_header.name(), lang_header.value())
-            .set(auth_header.name(), auth_header.value())
-            .timeout_connect(TIMEOUT)
-            .timeout_read(TIMEOUT)
-            .call();
+        let response = minreq::get(&full_url)
+            .with_header("Accept-Language", self.lang.to_string())
+            // TODO: Make this into an error
+            .with_header("Authorization", format!("Bearer {}", self.api_key.as_ref().expect("Guild Wars 2 API key is not set")))
+            .with_timeout(TIMEOUT)
+            .send()?;
         Client::handle_response(response)
     }
 
@@ -116,19 +123,17 @@ impl Client {
     /// respectively.
     fn handle_response<T>(response: Response) -> Result<T, ApiError>
     where T: DeserializeOwned {
-        if response.ok() {
-            Ok(response.into_json_deserialize::<T>().unwrap())
-        } else {
-            match response.status() {
-                // Forbidden
-                403 => Err(ApiError::new(response.into_json().unwrap())),
-                // Not Found
-                404 => Err(ApiError::new(response.into_json().unwrap())),
-                // Timeout
-                408 =>
-                Err(ApiError::new("Client timed out. Probably due to the official API being down.".to_string())),
-                _ => Err(ApiError::new(response.into_json().unwrap())),
-            }
+        match response.status_code {
+            // Ok
+            200 => Ok(response.json::<T>().unwrap()),
+            // Forbidden
+            403 => Err(ApiError::new(ApiErrorKind::Forbidden)),
+            // Not Found
+            404 => Err(ApiError::new(ApiErrorKind::NotFound)),
+            // Timeout
+            408 =>
+            Err(ApiError::new(ApiErrorKind::ApiTimeout)),
+            _ => Err(ApiError::new(ApiErrorKind::Custom(String::new()))),
         }
     }
 
@@ -141,13 +146,9 @@ impl Client {
         }
     }
 
-    /// Returns an `Option` to a reference of the `Localisation` enum object if given,
-    /// otherwise None is returned in the Option.
-    pub fn lang(&self) -> Option<&Localisation> {
-        match &self.lang {
-            Some(lang) => Some(&lang),
-            None => None,
-        }
+    /// Returns a reference to the `Localisation` enum object.
+    pub fn lang(&self) -> &Localisation {
+        &self.lang
     }
 }
 
@@ -165,6 +166,6 @@ mod tests {
             .to_string();
         let client = Client::new().set_api_key(api_key.clone()).set_lang(Localisation::French);
         assert_eq!(&api_key, client.api_key().unwrap());
-        assert_eq!(&Localisation::French, client.lang().unwrap());
+        assert_eq!(&Localisation::French, client.lang());
     }
 }
